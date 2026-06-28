fcd46512a3ea5aa108d2526900295fe36d1f74b5
MMunim <mmunim9.9.01@gmail.com>
2026-06-29 01:06:27 +0600

    js module start

---
+++ Module 5/app.js
@@ -0,0 +1,16 @@
+var test = 'Hello World';
+console.log(test);
+console.log(typeof(test));
+
+var num = 10;
+console.log(typeof(num));
+
+var status = true;
+console.log(typeof(status));
+
+
+var obj = {
+    name: "gias",
+}
+
+console.log(typeof(obj));
---
+++ Module 5/second.js
@@ -0,0 +1,7 @@
+var num1 = "12.5";
+var num2 = 15;
+
+var result = parseFloat(num1) + num2;
+
+console.log(result)
+console.log(typeof(result))
