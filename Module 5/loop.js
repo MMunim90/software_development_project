3e1588216f512ca218173eb64adcd87b23407e03
MMunim <mmunim9.9.01@gmail.com>
2026-06-29 17:55:30 +0600

    loop done

---
+++ Module 5/loop.js
@@ -0,0 +1,29 @@
+var test = "Test";
+
+var elements = [
+  "habib",
+  5,
+  "kobir",
+  test,
+  { name: "Jhon" },
+  ["rohim", "korim"],
+];
+
+// for(var i = 0; i < elements.length; i++){
+//     console.log(elements[i]);
+// }
+
+// for(var i = 0; i < elements.length; i++){
+//     var element = elements[i];
+
+//     if(element == "kobir"){
+//         console.log("Yes");
+//     }
+//     else{
+//         console.log("No");
+//     }
+// }
+
+for (var i = 0; i <= 20; i++) {
+  console.log(i);
+}
