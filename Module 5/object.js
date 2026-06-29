74aa5e61028f1c967cd8e3c6890090585d122e33
MMunim <mmunim9.9.01@gmail.com>
2026-06-29 17:18:50 +0600

    object done

---
+++ Module 5/object.js
@@ -0,0 +1,16 @@
+var person = {
+    hand : 2,
+    eye : 2,
+    hair: "can't calculate it",
+    friends : [
+        "rahim", "karim", "joshim"
+    ],
+    father : {
+        name : "Habib",
+        age : 45,
+    },
+}
+
+console.log(person.hand)
+console.log(person.father)
+console.log(person.father.name)
