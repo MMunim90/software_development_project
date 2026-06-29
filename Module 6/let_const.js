f4c911b6f592379be23064c899b6a2523336a435
MMunim <mmunim9.9.01@gmail.com>
2026-06-29 19:55:00 +0600

    let and const

---
+++ Module 6/let_const.js
@@ -0,0 +1,11 @@
+const num = 5;
+
+// num = 40; // if variable datatype is const, then reassignment in this variable is not possible.
+
+console.log(num);
+
+let name = "munim";
+
+name = "al munim";
+
+console.log(name);
