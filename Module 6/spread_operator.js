d36555091383b0cbac72ae6d879fe808f85f0ac0
MMunim <mmunim9.9.01@gmail.com>
2026-06-30 07:33:35 +0600

    template literals and spread operator

---
+++ Module 6/spread_operator.js
@@ -0,0 +1,8 @@
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const number2 = [11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+// console.log([...number, ...number2]);
+
+console.log(...number)
+console.log(...number)
---
+++ Module 6/templet litarels.js
@@ -0,0 +1,7 @@
+const countryName = "Bangladesh";
+
+// const country = "My country name is " + countryName + ". I love my country."  // this is concat
+
+const country = `My country name is ${countryName}`;
+
+console.log(country);
