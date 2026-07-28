6c8cd3a719054678cb1e35b3bfe4c5ffe14810c3
MMunim <mmunim9.9.01@gmail.com>
2026-07-28 21:52:04 +0600

    DOM loop if else

---
+++ Module 7/app.js
@@ -0,0 +1,26 @@
+alert("This is munim's workspace")
+
+// console.log(document.getElementsByTagName("h1"));
+
+// const target = document.getElementsByClassName("title")
+// console.log(target[0]);
+
+const target = document.getElementById("title");
+
+// console.log(target);
+
+target.style.color = "red";
+
+const allbox = document.getElementsByClassName("box");
+
+console.log(allbox);
+
+for(let i=0; i<allbox.length; i++){
+    const element = allbox[i];
+    // console.log(element)
+    element.style.backgroundColor = "green";
+
+    if(element.innerText == "box-5"){
+        element.style.backgroundColor = "red";
+    }
+}
