b5cc362accd6f6822ce0b63d3a5d7289d37984ee
MMunim <mmunim9.9.01@gmail.com>
2026-07-31 01:20:03 +0600

    api json placeholder 2

--- Module 7/app.js
+++ Module 7/app.js
@@ -1,4 +1,4 @@
-alert("This is munim's workspace")
+alert("This is munim's workspace");
 
 // console.log(document.getElementsByTagName("h1"));
 
@@ -15,68 +15,102 @@ const allbox = document.getElementsByClassName("box");
 
 // console.log(allbox);
 
-for(let i=0; i<allbox.length; i++){
-    const element = allbox[i];
-    // console.log(element)
-    element.style.backgroundColor = "green";
+for (let i = 0; i < allbox.length; i++) {
+  const element = allbox[i];
+  // console.log(element)
+  element.style.backgroundColor = "green";
 
-    if(element.innerText == "box-5"){
-        element.style.backgroundColor = "red";
-    }
+  if (element.innerText == "box-5") {
+    element.style.backgroundColor = "red";
+  }
 }
-
 
 // document.getElementById("handleADD").addEventListener("click", (event)=>{
 //     console.log("Hello Boss");
 // })
 
+const handleSearch = (event) => {
+  // console.log("Hello Boss");
 
-const handleSearch = (event)=>{
-    // console.log("Hello Boss");
+  const inputValue = document.getElementById("search-Box").value;
 
-    const inputValue = document.getElementById("search-Box").value;
+  // console.log(inputValue);
 
-    // console.log(inputValue);
+  const container = document.getElementById("comment-container");
 
-    const container = document.getElementById("comment-container");
+  const p = document.createElement("p");
+  p.classList.add("child");
+  p.innerText = inputValue;
 
-    const p=document.createElement("p");
-    p.classList.add("child");
-    p.innerText = inputValue;
+  // console.log(p.innerText);
 
-    // console.log(p.innerText);
+  container.appendChild(p);
 
-    container.appendChild(p);
-    
-    document.getElementById("search-Box").value = "";
+  document.getElementById("search-Box").value = "";
 
-    // console.log(container);
+  // console.log(container);
 
-    const allComments = document.getElementsByClassName("child");
+  const allComments = document.getElementsByClassName("child");
 
-    // console.log(allComments)
-    // using for of loop
-    for(const element of allComments){
-        // console.log(element)
+  // console.log(allComments)
+  // using for of loop
+  for (const element of allComments) {
+    // console.log(element)
 
-        element.addEventListener("click", (e)=>{
-            // console.log(e.target);
-            // console.log(e.target.parentNode);
+    element.addEventListener("click", (e) => {
+      // console.log(e.target);
+      // console.log(e.target.parentNode);
 
-
-            // three way to remove child from parent
-            // e.target.parentNode.removeChild(element);
-            // or this can be write as,
-            // e.target.remove();
-            // or
-            element.remove()
-        })
-    }
-}
-
+      // three way to remove child from parent
+      // e.target.parentNode.removeChild(element);
+      // or this can be write as,
+      // e.target.remove();
+      // or
+      element.remove();
+    });
+  }
+};
 
 fetch("https://jsonplaceholder.typicode.com/users")
-.then(res => res.json())
-.then(data => {
-    console.log(data);
-})
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log(data);
+    displayData(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const displayData = (userData) => {
+  const container = document.getElementById("userData-Container");
+
+  userData.forEach((user) => {
+    console.log(user);
+    const div = document.createElement("div");
+    div.classList.add("user");
+
+    div.innerHTML = `
+        <h2>${user.name}</h2>
+                    <p>ID: ${user.id}</p>
+                    <p>Username: ${user.username}</p>
+                    <p>Email: ${user.email}</p>
+                    <p>Phone: ${user.phone}</p>
+                    <p>Website: ${user.website}</p>
+
+                    <h3>Address</h3>
+                    <p>Street: ${user.address.street}</p>
+                    <p>Suite: ${user.address.suite}</p>
+                    <p>City: ${user.address.city}</p>
+                    <p>Zipcode: ${user.address.zipcode}</p>
+
+                    <h3>Company</h3>
+                    <p>Company Name: ${user.company.name}</p>
+                    <p>Catch Phrase: ${user.company.catchPhrase}</p>
+                    <p>Business: ${user.company.bs}</p>
+
+                    <hr>
+        `;
+
+    container.appendChild(div);
+  });
+};
